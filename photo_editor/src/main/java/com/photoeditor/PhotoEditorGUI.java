cbe5fab1ab3e46b21d37b6192728c685f9430af0
Brandon Bouley <bouleyb@merrimack.edu>
2024-02-25 15:05:45 -0500

    Foundations of the cards

--- photo_editor/src/main/java/com/photoeditor/PhotoEditorGUI.java
+++ photo_editor/src/main/java/com/photoeditor/PhotoEditorGUI.java
@@ -238,11 +238,22 @@ public class PhotoEditorGUI extends JFrame {
     JPanel mainPanel = new JPanel();
     mainPanel.setLayout(new BorderLayout());
 
+    JPanel cards;
+    String item1 = "item1";
+    String item2 = "item2";
+
+    String comboBoxItems[] = {item1, item2};
+    JComboBox comboBox = new JComboBox(comboBoxItems);
+
+    JPanel card1 = new JPanel();
+    JPanel card2 = new JPanel();
+
     // Top panel
-    JPanel topPanel = new JPanel();
+    JPanel topPanel = new JPanel(new CardLayout());
     topPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
     topPanel.add(saveButton);
     topPanel.add(loadButton);
+    
     undoButton.addActionListener(
       new ActionListener() {
         @Override
@@ -264,6 +275,7 @@ public class PhotoEditorGUI extends JFrame {
     );
 
     topPanel.add(undoButton);
+    topPanel.add(comboBox);
 
     mainPanel.add(topPanel, BorderLayout.NORTH);
     toolStatusLabel = new JLabel("Selected Tool: " + sidebarStatus);
